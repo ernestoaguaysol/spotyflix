2250e124c752908b6b95ab64e3614dd0b167e9d1
Ernesto <ernestoaguaysol@gmail.com>
2017-11-03 20:45:27 -0300

    class main and component header

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { MenuComponent } from './common/menu/menu.component';
+import { HeaderComponent } from './common/header/header.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    MenuComponent
+    MenuComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule
